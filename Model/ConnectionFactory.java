2283c1847a1acf9081182358faf56b38330bec75
gabrielruy <happyarcsestra@gmail.com>
2019-06-26 21:28:49 -0300

    Adição de manual para execução

--- Model/ConnectionFactory.java
+++ Model/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	
 	public Connection getConnection() {
 		try {
-			return DriverManager.getConnection("jdbc:hsqldb:hsql://127.0.0.1:9137/myServerDB", "sa", "");
+			return DriverManager.getConnection("jdbc:hsqldb:hsql://127.0.0.1:9137/myserverdb", "sa", "");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
---
+++ database/bin/runServerMyServerDB.bat
@@ -0,0 +1,2 @@
+cd ..\data
+@java -classpath ../../lib/hsqldb.jar org.hsqldb.server.Server -database.0 ../data/myServerDB -dbname.0 myserverdb -port 9137
